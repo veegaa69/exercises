1e8fd1713522037779617fd7a1e841bd4a0500b6
veegaa69 <beatrizyuassa@gmail.com>
2022-02-16 12:06:04 -0300

    up

---
+++ projeto_battle/rascunho.js
@@ -0,0 +1,14 @@
+
+function quadrado(numero) {
+  console.log(numero * numero);
+
+}
+
+quadrado(10);
+
+function soma(numeroA, numeroB) {
+  const resultadoSoma = numeroA + numeroB;
+  return resultadoSoma;
+}
+
+console.log(soma(5,2));
